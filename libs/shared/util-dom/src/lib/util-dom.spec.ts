c15059e084f89b77fc504d1b6eaa01a64aa1fa2e
Gui Seek <guiseek@gmail.com>
2023-07-16 17:26:45 -0300

    feat: create shared util-dom library

---
+++ libs/shared/util-dom/src/lib/util-dom.spec.ts
@@ -0,0 +1,7 @@
+import {sharedUtilDom} from './util-dom'
+
+describe('sharedUtilDom', () => {
+  it('should work', () => {
+    expect(sharedUtilDom()).toEqual('shared-util-dom')
+  })
+})
---
+++ libs/shared/util-dom/src/lib/util-dom.ts
@@ -0,0 +1,3 @@
+export function sharedUtilDom(): string {
+  return 'shared-util-dom'
+}
